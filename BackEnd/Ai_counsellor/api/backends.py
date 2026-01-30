bee139986846c9f648fd0c0f4d285c01a67b00ef
pyaishark <imfraw@BOOK-9M8IV2LTA0.localdomain>
2026-01-30 17:34:11 +0000

    email problem solved

--- BackEnd/Ai_counsellor/api/backends.py
+++ BackEnd/Ai_counsellor/api/backends.py
@@ -8,21 +8,4 @@ class SMTP4(smtplib.SMTP):
         return socket.create_connection((host, port), timeout, source_address=None)
 
 class CustomEmailBackend(EmailBackend):
-    def open(self):
-        """
-        Ensures we have a connection to the email server. Returns whether or not
-        a new connection was created (or already existed).
-        """
-        if self.connection:
-            # Nothing to do if the connection is already open.
-            return False
-
-        # If we need to connect, we simply override self.connection_class
-        # temporarily or just manually instantiate our custom class.
-        # But Django's EmailBackend uses self.connection_class which defaults to smtplib.SMTP.
-        # We can just set it on the instance or class.
-        
-        # We will use our custom SMTP4 class that forces IPv4
-        self.connection_class = SMTP4
-        
-        return super().open()
+    connection_class = SMTP4
